b35017344069fc149e8f5f6de6421a5433510ac8
Shinrael <mircoelena89@gmail.com>
2024-02-22 16:09:01 +0100

    [add] word Fizz and Buzz

--- js/script.js
+++ js/script.js
@@ -1,33 +1,29 @@
 // Tramite FOR genero numeri da 1 a 100
 
 const boxQ = document.getElementById('box-q');
+let write = '';
 
 for(let i = 1; i <= 100; i++){
   let word = i;
- 
-  // Vicino ai multipli di 3 metto la parola Fizz
+  boxQ.innerHTML = write;
 
-  if(i % 3 === 0){
-    boxQ.innerHTML += `
-    <div class="box">${'Fizz'}</div>
-  `  
-  // Vicino ai multipli di 5 metto la parola Buzz
-  
-  }else if(i % 5 === 0){
-    boxQ.innerHTML += `
-    <div class="box">${'Buzz'}</div>
-  `  
+  // Dentro i multipli di 15 metto la parola FizzBuzz
+
+  if (i % 15 === 0) {
+    write += '<div class="box">FizzBuzz</div>';
+
+    // Dentro i multipli di 3 metto Fizz
+
+  } else if (i % 3 === 0) {
+    write += '<div class="box">Fizz</div>';
+
+    // Dentro i multipli di 5 metto Buzz
+  } else if (i % 5 === 0) {
+    write += '<div class="box">Buzz</div>';
+
+    // Altrimenti utilizzo i numeri
   } else {
-    boxQ.innerHTML += `
-    <div class="box">${word}</div>
-  `  
+    write += `<div class="box">${word}</div>`;
   }
-  
-
-  
-
-  
-  
-
 }
 
